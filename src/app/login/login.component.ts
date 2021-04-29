e67222c7f1a8ab53f0fe05fba8eecda0c9296dd0
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-04-29 00:52:03 -0300

    implemented login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import {Router} from "@angular/router"
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user = {user: '', pwd: ''};
+
+  constructor(public dataService: DataService, private router: Router) { }
 
   ngOnInit(): void {
+    if(document.cookie)
+      this.router.navigate(['/home']);
+  }
+
+  login(): void {
+    // Process checkout data here
+    this.dataService.login(this.user).subscribe((res) => {
+      var now = new Date();
+      var time = now.getTime();
+      time += 3600 * 1000;
+      now.setTime(time);
+      document.cookie = `token=${res.token}; expires=${now.toUTCString()};`;
+      this.router.navigate(['/home'])
+    });
+    
   }
 
 }
